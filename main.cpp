3631e0eca65209e317158068eaab987cc7e8e591
Quentin RICHERT <quentin.richert@gmail.com>
2010-09-01 20:21:37 +0200

    Finishing update system.

--- main.cpp
+++ main.cpp
@@ -37,8 +37,7 @@ int main(int argc, char *argv[])
 	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 	
 	QCoreApplication::setApplicationName("Multiuso");
-	QCoreApplication::setApplicationVersion("2.2.0");
-	//QCoreApplication::setApplicationVersion("2.2.1");
+	QCoreApplication::setApplicationVersion("2.2.1");
 	QCoreApplication::setOrganizationName("Quentin RICHERT");
 	QCoreApplication::setOrganizationDomain("http://multiuso.sourceforge.net/");
 
